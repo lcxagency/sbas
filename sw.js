5092d5808d2ba02d3d71cce9f10647ac5f4687bf
lcxagency <contact@lcxagency.com>
2026-04-03 00:08:49 +0200

    Update service worker cache version and assets

--- sw.js
+++ sw.js
@@ -1,13 +1,29 @@
-const CACHE = 'sbas-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
+const CACHE = 'sbas-v2';
+const ASSETS = [
+  '/',
+  '/sbas/',
+  '/sbas/index.html',
+  '/sbas/manifest.json'
+];
 
 self.addEventListener('install', e => {
-    e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-    self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-    e.respondWith(
-          caches.match(e.request).then(r => r || fetch(e.request))
-        );
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
 });
